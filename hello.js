0cf9877a30eae7f6436147ba4c1448aab79de112
Christian Araujo <araujo.christian@gmail.com>
2024-01-09 22:41:19 -0600

    Updating message

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola MIT desde Guatemala, haciendo GitHub actions";
 console.log(hello());
 exports.hello = hello;
